9711c0b3f36248bcd63f4b4fd277f944ffdb19e7
jiahangLee <lijiahang997>
2018-12-15 23:01:34 +0800

    @RequestBody让参数接收json

--- src/main/java/com/jiahanglee/journey/controller/ConfessionController.java
+++ src/main/java/com/jiahanglee/journey/controller/ConfessionController.java
@@ -4,7 +4,14 @@ import com.jiahanglee.journey.entity.Confession;
 import com.jiahanglee.journey.services.ConfessionService;
 import com.jiahanglee.journey.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * code is far away from bug with the animal protecting
@@ -31,6 +38,7 @@ import org.springframework.web.bind.annotation.*;
  * @date 2018/10/9 18:07
  * @description //TODO
  */
+@CrossOrigin
 @RestController
 //@RequestMapping(value = "/user")
 public class ConfessionController {
@@ -39,12 +47,25 @@ public class ConfessionController {
     private ConfessionService confessionService;
 
     @GetMapping("/confession/{id}")
-    public Object selectConfession(@PathVariable(value = "id",required = false) Integer id){
+    public Object selectConfession(@PathVariable(value = "id", required = false) Integer id) {
         return confessionService.selectConfession(id);
     }
+
     @PostMapping("/confession")
-    public String createConfession(Confession confession){
-        return confessionService.createConfession(confession);
+    public String createConfession(@RequestBody Confession confession, HttpServletResponse httpServletResponse) {
+        System.out.println(confession.toString());
+        String result = confessionService.createConfession(confession);
+        Map<String, String> map = new HashMap<>();
+        map.put("loveUrl", result);
+        return String.valueOf(new JSONObject(map));
+//        try {
+//            Map<String, String> map = new HashMap<>();
+//            map.put("loveUrl", result);
+//            PrintWriter writer = httpServletResponse.getWriter();
+//            writer.write(String.valueOf(new JSONObject(map)));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
 
